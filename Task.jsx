9eaf994e059352c9cb32b67657278e11b9980408
Nathan Allison <nate.allison@gmail.com>
2015-09-23 17:28:41 -0600

    add completion and removal

--- Task.jsx
+++ Task.jsx
@@ -5,9 +5,37 @@ Task = React.createClass({
     //can use PropTypes to indicate it is require
     task: React.PropTypes.object.isRequired
   },
+
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: {checked: ! this.props.task.checked}
+    });
+  },
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  },
+
   render() {
+
+    const taskClassName = this.props.task.checked ? "checked" : "";
+
     return (
-      <li> {this.props.task.text}</li>
+      <li className={taskClassName}>
+
+        <button className="delete" onClick={this.deleteThisTask}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly={true}
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked} />
+
+        <span className="text">{this.props.task.text}</span>
+
+      </li>
     );
   }
 });
